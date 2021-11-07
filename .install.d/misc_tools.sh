1d712c2f437707c71d99910ffdaeb7be7b017932
Konstantin Tretyakov <konstantin.tretjakov@gmail.com>
2021-11-07 16:16:45 +0000

    Fixes to misc_tools.sh

--- .install.d/misc_tools.sh
+++ .install.d/misc_tools.sh
@@ -1,12 +1,13 @@
 for tool in htop ranger ncdu cowsay lolcat prettyping; do
     which $tool || sudo apt install -y $tool;
 done
-which batcat || sudo apt install -y bat
-which vidir || sudo apt install -y moretools
+# https://askubuntu.com/a/1300824
+which batcat || sudo apt install -y -o Dpkg::Options::="--force-overwrite" bat
+which vidir || sudo apt install -y moreutils
 which rg || sudo apt install -y ripgrep
 
 
-if ! which lazygit >& /dev/null;
+if ! which lazygit >& /dev/null; then
   if which add-apt-repository >& /dev/null; then
     sudo add-apt-repository ppa:lazygit-team/release
     sudo apt-get update
