bd9c3f76a1a79cb7c6dff213dd9edce81b19b780
Konstantin Tretyakov <konstantin.tretjakov@gmail.com>
2021-11-10 23:41:50 +0000

    Install fzf on Ubuntu 18.04 machines as well

--- .bashrc.d/fzf.sh
+++ .bashrc.d/fzf.sh
@@ -10,3 +10,9 @@ alias preview="fzf --preview 'batcat --color \"always\" {}'"
 # add support for ctrl+o to open selected file in Vim 
 export FZF_DEFAULT_OPTS="--bind='ctrl-o:execute(vim {})+abort' --border --height=50%"
 
+# For Ubuntu 18.04:
+if [ -z "$BASH" ]; then
+    [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+else
+    [ -f ~/.fzf.bash ] && source ~/.fzf.bash
+fi
--- .install.d/fzf.sh
+++ .install.d/fzf.sh
@@ -1,2 +1,10 @@
-which fzf || sudo apt install -y fzf
-
+if ! which fzf; then
+    if grep "Ubuntu 18" /etc/lsb-release; then
+        git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+        ~/.fzf/install --key-bindings --completion --no-update-rc
+        mkdir -p ~/.local/bin
+        ln -sf ~/.fzf/bin/fzf ~/.local/bin/fzf
+    else
+        sudo apt install -y fzf
+    fi
+fi
