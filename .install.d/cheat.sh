905ab22ae4e98bec9f3e857c363fc19a43511dbf
Konstantin Tretyakov <konstantin.tretjakov@gmail.com>
2023-05-15 11:32:10 +0200

    Added the cheat tool.

---
+++ .install.d/cheat.sh
@@ -0,0 +1,13 @@
+#zsh-compatible
+
+# https://github.com/cheat/cheat
+# Look up latest version at https://github.com/cheat/cheat/releases
+
+which cheat || {
+    wget https://github.com/cheat/cheat/releases/latest/download/cheat-linux-amd64.gz -O /tmp/cheat-linux-amd64.gz \
+    && gunzip /tmp/cheat-linux-amd64.gz \
+    && chmod +x /tmp/cheat-linux-amd64 \
+    && mv /tmp/cheat-linux-amd64 ~/.local/bin/cheat \
+    && rm -f /tmp/cheat-linux-amd64.gz
+}
+
