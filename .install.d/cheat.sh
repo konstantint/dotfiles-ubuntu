7a87c9b063a6881e62e1dd7df5a7a0b92084223c
Konstantin Tretyakov <konstantin.tretjakov@gmail.com>
2023-05-15 12:17:47 +0200

    Creating .local/bin directory when installing cheat.sh

--- .install.d/cheat.sh
+++ .install.d/cheat.sh
@@ -7,6 +7,7 @@ which cheat || {
     wget https://github.com/cheat/cheat/releases/latest/download/cheat-linux-amd64.gz -O /tmp/cheat-linux-amd64.gz \
     && gunzip /tmp/cheat-linux-amd64.gz \
     && chmod +x /tmp/cheat-linux-amd64 \
+    && mkdir -p ~/.local/bin \
     && mv /tmp/cheat-linux-amd64 ~/.local/bin/cheat \
     && rm -f /tmp/cheat-linux-amd64.gz
 }
